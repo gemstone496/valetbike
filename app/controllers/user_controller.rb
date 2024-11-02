09d4854c0870b5c80d5a8142451655798d670ef1
Lynn Do <do24l@mtholyoke.edu>
2024-11-02 14:30:10 -0400

    Add reset password and log in

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,11 @@
 class UserController < ApplicationController
     def new 
-        """
-        Display the sign up/log in form
-        """
+        # Display the sign up form
         @user = User.new
     end
 
     def create 
-        """
-        Process log in information
-        """
+        # Process sign up information
         @user = User.new(user_params)
         if @user.save 
             cookies[:username] = user_params[:email]
@@ -18,28 +14,14 @@ class UserController < ApplicationController
         else 
             render :new, status: 422
         end
-    end 
+    end  
 
     private 
 
     def user_params
-        """
-        Return params that includes only the given fields 
-        and sets the permitted attribute for the object to true.
-        (useful for limiting which attributes should be allowed for updating)
-        """
+        # Return params that includes only the given fields 
+        # and sets the permitted attribute for the object to true.
+        # (useful for limiting which attributes should be allowed for updating)
         params.require(:user).permit(:email, :password, :password_confirmation)
-    end
-
-    def fetch_user_info
-        @username = cookies[:username]
-        @user_id = session[:user_id]
-    end
-
-    def logged_in?
-        """
-        Check user's log in status
-        """
-        session[:user_id].present?
     end
 end
