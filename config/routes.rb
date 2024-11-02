731ba71f16a35cd4b8d2c8b26002f2ee09e14945
Lynn Do <do24l@mtholyoke.edu>
2024-11-02 16:17:13 -0400

    Gemfile doesn't work. Have to change it

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   get "password/reset/edit", to: "password_resets#edit"
   patch "password/reset/edit", to: "password_resets#update"
 
-  root to: "stations#index"
   get 'main/pricing'
   get 'main/account'
   get 'main/contact'
