2e28914a9f96855a7a15276baf82e7463f86b269
Jade Lilian <jpalosky@smith.edu>
2024-11-03 18:00:51 -0500

    Remove insecure password field from the user table in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_01_172048) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email", null: false
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
