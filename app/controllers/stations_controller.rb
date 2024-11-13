4076a0f2f8f6551d9b0d72a61903a11704118b0c
Jade Lilian <jpalosky@smith.edu>
2024-11-12 21:06:45 -0500

    setup lat and long arrays in stations/index for easier passing

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,6 +2,12 @@ class StationsController < ApplicationController
   
   def index
     @stations = Station.all.order(identifier: :asc)
+    @lats = Array.new # array of latitudes
+    @longs = Array.new # array of longitudes
+    @stations.each do |s|
+      @lats.append(s.latitude)
+      @longs.append(s.longitude)
+    end
   end
   
 end
