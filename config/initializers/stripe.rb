36a819a490cbcac4b20cb15b97670f8b01566bc0
Jade Lilian <jpalosky@smith.edu>
2024-11-22 17:41:16 -0500

    add missed config line

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,3 +2,4 @@ Rails.configuration.stripe = {
     publishable_key: ENV["pk_test_51QO2idGS1H69W3qmjkkpKiGssJ5KL1blHkO6YqLjFAuulDXjidDIWP7zCEIyrE31HpfuV2jEvQtCO81FqaF7NjOQ00Z2UdLvzh"],
     secret_key: ENV["sk_test_51QO2idGS1H69W3qmVa5Frqv4dWhXfF3IdXm66sc4eWSR0NAunNeMbesHEjXJAnCBvkckIgFQCzLjF1ZiQ0zCGAOP00FIMVnUPv"]
 }
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
