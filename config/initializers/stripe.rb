5bceb3f926e19c98966d763f62a030578f4ac926
Lynn Do <do24l@mtholyoke.edu>
2024-12-05 13:05:06 -0500

    Configure Stripe API keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,1 @@
-Rails.configuration.stripe = {
-    publishable_key: ENV["pk_test_51QO2idGS1H69W3qmjkkpKiGssJ5KL1blHkO6YqLjFAuulDXjidDIWP7zCEIyrE31HpfuV2jEvQtCO81FqaF7NjOQ00Z2UdLvzh"],
-    secret_key: ENV["sk_test_51QO2idGS1H69W3qmVa5Frqv4dWhXfF3IdXm66sc4eWSR0NAunNeMbesHEjXJAnCBvkckIgFQCzLjF1ZiQ0zCGAOP00FIMVnUPv"]
-}
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.application.credentials[:stripe][:secret]
