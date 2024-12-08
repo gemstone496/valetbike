999bb0fc8511181edf9240628748ac94c9deda80
Lynn Do <do24l@mtholyoke.edu>
2024-12-07 21:46:25 -0500

    Add trips to mobile nav and fix leaflet default pfp url

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -7,16 +7,9 @@ export default class extends Controller {
     lat: Array,
     long: Array,
     name: Array,
-    identifier: Array,
     id: Array,
-    address: Array,
-    bike: Array,
-    userid: Boolean,
-    tripid: Boolean,
-    iduser: String,
-    idtrip: String,
-    userCoords: {type: Array, default: ['42.3172951', '-72.6386734'] },
-    pfp: {type: String, default: 'assets/fallback/default.png'}
+    userCoords: {type: Array, default: ['42.3255', '-72.646'] },
+    pfp: {type: String}
   };
 
   connect(){
@@ -34,12 +27,9 @@ export default class extends Controller {
       let lat = this.latValue[i];
       let long = this.longValue[i];
       let name = this.nameValue[i];
-      let identifier = this.identifierValue[i];
       let id = this.idValue[i];
-      let address = this.addressValue[i];
-      let bike = this.bikeValue[i];
       if (lat !== null && long !== null && name !== null) {
-        this.addMarker(lat, long, name, identifier, id, address, bike, purpleIcon);
+          this.addMarker(lat, long, name, id, purpleIcon);
       }
     }
   }
@@ -68,10 +58,8 @@ export default class extends Controller {
     console.log("Map Loaded!!");
   }
 
-  /* adds a marker to the leaflet
-  TODO
-    - link popup to the station page */
-  addMarker(lat, long, name, identifier, id, address, bike, iconPath) {
+  /* adds a marker to the leaflet */
+  addMarker(lat, long, name, id, iconPath) {
 
     let size = id === null ? [30, 30] : [30, 40]; // size of the icon
 
@@ -81,70 +69,18 @@ export default class extends Controller {
     });
 
         // Place a marker on the location with custom icon
-    let marker = L.marker([lat, long], {icon: icon}).addTo(this.map);
-    if (id !== null) { // here markers
-      marker.bindPopup(this.popupContent(name, identifier, id, address, bike));
+    let marker = L.marker([lat, long], {icon: icon})
+        .addTo(this.map);
+
+    if (id !== null){ // here markers
+      marker.bindPopup(this.buttonTo(name, id));
     }
   }
 
   /* helper method to get the button_to link to place in the popup */
-/*  popupContent(name, id, address, bike) {
-    return `
-    <div>
-     <div class="title">  Station ${id}: ${name} </div> <br>
-    <div class="body"> ${address} </div> 
-    <div class="body"> ${bike} available bikes </div> 
-    <form class='button_to' method='get' action='/stations/${id}'> 
-        <button class='mt-3 custom-btn btn-outline' type='submit'>Reserve a Bike</button>
-      </form>
-      </div>
-  `;
-  }*/
-  popupContent(name, identifier, id, address, bike) {
-    const availableBikes = bike;
-    const user = this.useridValue;
-    const trip = this.tripidValue;
-    const user_id = this.iduserValue;
-    let buttonContent = '';
-
-      if (!user) {
-        buttonContent = `
-        <button onclick="reserve_alert('Please log in to reserve a bike!')" 
-        class="mt-3 custom-btn btn-invalid">Reserve Bike</button>
-      `;
-
-      } else if (availableBikes == 0 && !trip) {
-        buttonContent = `
-        <button onclick="reserve_alert('No bikes at this station!')" 
-        class="mt-3 custom-btn btn-invalid">Reserve Bike</button>
-      `;
-      } else if (!trip) {
-      buttonContent = `
-        <a href="/trips/new?station_id=${id}&user_id=${user_id}" class="mt-3 custom-btn btn-fill" 
-        style="color: unset; text-decoration: unset;">
-      Reserve Bike
-    </a>
-      `;
-    } else if (trip) {
-      buttonContent = `
-       <a href="/end_confirm?end_station_id=${id}&user_id=${user_id}" class="mt-3 custom-btn btn-fill" 
-       style="color: unset; text-decoration: unset;">
-      Return Bike Here
-    </a>
-      `;
-    }
-    return `
-      <div>
-        <div class="title">Station ${identifier}: ${name}</div><br/>
-        <div class="body">${address}</div>
-        <div class="body">${availableBikes} available bikes</div><br>
-        ${buttonContent}
-      </div>
-      <script>
-        function reserve_alert(message) {
-          alert(message);
-        }
-      </script>
-    `;
+  buttonTo(name, id) {
+    return `<form class='button_to' method='get' action='/stations/${id}'> 
+      <button class='mt-3 custom-btn btn-outline' type='submit'>${name}
+      </button></form>`
   }
 }
